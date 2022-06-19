4ac467484a0484eb93f2c145aec25c6e3bc5d478
Bojana Karanovic <bojana.karanovic2015@gmail.com>
2022-06-19 23:27:14 +0200

    Uradjena pretraga

--- WebShopREST/WebContent/components/sportsFacilities.js
+++ WebShopREST/WebContent/components/sportsFacilities.js
@@ -1,16 +1,27 @@
 Vue.component("sportsFacilities", { 
 	data: function () {
 	    return {
-	      sportsFacilities: null
+	      sportsFacilities: null,
+	      searched:null,
+	      name:null,
+	      city:null,
+	      rating:null,
+	      type:null
 	    }
-	},
+	}
+	,
 	    template: ` 
     	<div>
     		<h3>Prikaz sportskih objekata</h3>
-    		<p><input type="text" id="nameToCheck"></p>
-
-			<button onclick="myFunction()">Pretrazi</button>
-    		<table border="1" id="tableSportsFacilities">
+    		<input type="text" v-model="this.name">
+			<button v-on:click="searchName(this.name)">Pretrazi po imenu</button>
+			<input type="text" v-model="this.city">
+			<button v-on:click="searchLocation(this.city)">Pretrazi po gradu</button>
+			<input type="text" v-model="this.rating">
+			<button v-on:click="searchRating(this.rating)">Pretrazi po oceni</button>
+			<input type="text" v-model="this.type">
+			<button v-on:click="searchType(this.type)">Pretrazi po tipu</button>
+			<table border="1" id="tableSportsFacilities" v-bind:sportsFacilities = "this.searched">
 	    		<tr bgcolor="lightgrey">
 	    			<th>Naziv</th>
 	    			<th>Tip objekta</th>
@@ -22,7 +33,7 @@ Vue.component("sportsFacilities", {
 	    			<th>Radno vreme</th>
 	    		</tr>
 	    			
-	    		<tr v-for="sportFacility in sportsFacilities">
+	    		<tr v-for="sportFacility in searched">
 	    			<td id = "name">{{sportFacility.name}}</td>
 	    			<td>{{sportFacility.type}}</td>
 	    			<td>{{sportFacility.trainingType}}</td>
@@ -39,22 +50,30 @@ Vue.component("sportsFacilities", {
 	    			
 	    		</tr>
 	    	</table>
-	    	<script>
-	    		function checkName(name) {
- 				 return name === document.getElementById("sportFacility.name").value;
-				}
-				function myFunction() {
-				  document.getElementById("tableSportsFacilities").innerHTML = sportsFacilities.filter(checkName);
-				}
-	    	</script>
     		</div>		  
     	`,
     mounted () {
         axios
           .get('rest/sportsFacilities/')
-          .then(response => (this.sportsFacilities = response.data))
+          .then(response => (this.sportsFacilities = response.data, this.searched = response.data))
     },
     methods: {
+		searchName: function(name){
+			this.searched = this.sportsFacilities.filter(o => o.name.toLowerCase().includes(name.toLowerCase()));
+			
+		},
+		searchLocation: function(city){
+			this.searched = this.sportsFacilities.filter(o => o.location.address.city.toLowerCase().includes(city.toLowerCase()));
+			
+		},
+		searchRating: function(rating){
+			this.searched = this.sportsFacilities.filter(o => o.averageRating.toString().includes(rating.toString()));
+			
+		},
+		searchType: function(type){
+			this.searched = this.sportsFacilities.filter(o => o.type.toString().toLowerCase().includes(type.toString().toLowerCase()));
+			
+		}
     	/*addProduct : function() {
     		router.push(`/products/-1`);
     	},
