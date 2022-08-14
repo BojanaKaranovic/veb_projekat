624ad1045525a5d067e6db013037b68f241bde40
Bojana Karanovic <bojana.karanovic2015@gmail.com>
2022-08-14 16:59:55 +0200

    Stil za loginPage

--- WebShopREST/WebContent/components/user.js
+++ WebShopREST/WebContent/components/user.js
@@ -6,15 +6,29 @@ Vue.component("user", {
 	      password : ""
 	    }
 	},template: ` 
-    	<form id="forma">
-		<table>
-			<tr><td>Username</td><td><input v-model="username" type="text" name="username"></td></tr>
-			<tr><td>Password</td><td><input v-model="password" type="password" name="password"></td></tr>
-			<tr><td><button v-on:click = "loginUser">Login</button></td></tr>
-		</table>
-		<p id="error" hidden="true"></p>
+    	
+    	<div class="container" align="center">
+    		</br>
+    		<div>
+    			<img class="mb-4" src="images/logo.png" alt="" width="35%">
+    		</div>
+	    	<div class="col-md-3" align="left">
+		        <label class="form-label" style="color:#FFFFFF;" for="inputUsername">Korisnicko ime</label>
+		        <input v-model="username" type="text" class="form-control" id="inputUsername" placeholder="Username">
+		    </div>
+		    <div class="col-md-3 " align="left">
+		        <label class="form-label" style="color:#FFFFFF;" for="inputPassword">Lozinka</label>
+		        <input  max-width="330px"  padding="15px" v-model="password" type="password" class="form-control" id="inputPassword" placeholder="Password">
+		    </div>
+		    </br>
+		    <div class="col-md-3" align="center" >
+		    	<button v-on:click = "loginUser" type="submit"  class="btn btn-primary">Uloguj se</button>
+		    </div>
+		    <p id="error" hidden="true"></p>
 		<p id="success" hidden="true"></p>
-	</form>
+		</div>
+		
+	
     	`,mounted () {
         /*axios
           .get('rest/login/'+this.username+'''password')
