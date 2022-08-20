99df5fbfe0ede1d854895938ec7751b1fd57b903
Dusko Radicic <dusko.radicic1@gmail.com>
2022-08-20 22:24:32 +0200

    info o objektima odradjen

---
+++ WebShopREST/WebContent/components/coach/sportsFacilityInfo.js
@@ -0,0 +1,53 @@
+Vue.component("sportsFacilityInfo", { 
+	data: function () {
+	    return {
+	      sportsFacility: null,
+	      name:'',
+	    }
+	}
+	,
+	    template: ` 
+    	<div>
+    		<h3>Informacije o sportskom objektu</h3>
+    		
+			<table border="1">
+	    		<tr bgcolor="lightgrey">
+	    			<th>Naziv</th>
+	    			<th>Tip objekta</th>
+	    			<th>Status</th>
+	    			<th>Lokacija</th>
+	    			<th>Logo</th>
+	    			<th>Prosecna ocena</th>
+	    			<th>Radno vreme</th>
+	    		</tr>
+	    			
+	    		<tr>
+	    			<td v-if="sportsFacility">{{sportsFacility.name}}</td>
+	    			<td v-if="sportsFacility">{{sportsFacility.type}}</td>
+	    			<td v-if="sportsFacility.status">Radi</td>
+	    			<td v-if="!sportsFacility.status">Ne radi</td>
+	    			<td v-if="sportsFacility">{{sportsFacility.location.address.street}} {{sportsFacility.location.address.number}}, {{sportsFacility.location.address.city}}, {{sportsFacility.location.address.zipCode}}</td>
+	    			<td v-if="sportsFacility"><img v-bind:src=" 'images/'+sportsFacility.logo " /> </td>
+	    			<td v-if="sportsFacility.averageRating">{{sportsFacility.averageRating}}</td>
+	    			<td v-if="!sportsFacility.averageRating">Nema ocena</td>
+	    			<td v-if="sportsFacility">{{sportsFacility.workTime}}</td>
+	    			
+	    			
+	    		</tr>
+	    	</table>
+    		</div>		  
+    	`,
+    mounted () {
+	this.name = this.$route.params.id;
+	if(this.name != ''){
+        axios
+          .get('rest/sportsFacilities/' + this.name)
+          .then(response => (this.sportsFacility = response.data))
+          }
+          else{
+	alert("Nesto ne radi")
+}
+    }
+    	
+    
+});
--- WebShopREST/WebContent/js/managerApp.js
+++ WebShopREST/WebContent/js/managerApp.js
@@ -1,10 +1,12 @@
 const Profile = { template: '<viewProfile></viewProfile>' }
 const SportsFacilities = {template: '<sportsFacilities></sportsFacilities>'}
+const SportsFacilityInfo = { template: '<sportsFacilityInfo></sportsFacilityInfo>' }
 const router = new VueRouter({
 	mode: 'hash',
 	  routes: [
 		{ path: '/', name: 'home', component: SportsFacilities},
-		{ path: '/profile', component: Profile}
+		{ path: '/profile', component: Profile},
+		{ path: '/sportsFacilityInfo/:id', component: SportsFacilityInfo}
 		
 		
 		]
